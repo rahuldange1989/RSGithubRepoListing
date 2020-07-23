6ebbb59854059defd613a171ed18b5db3e53e982
Rahul <rahul.dange1989@gmail.com>
2020-07-23 15:39:08 +0530

    Created iOS Frameword project

---
+++ RSGithubRepoListing/RSGithubRepoListing/RSGithubRepoListing.h
@@ -0,0 +1,19 @@
+//
+//  RSGithubRepoListing.h
+//  RSGithubRepoListing
+//
+//  Created by Rahul Dange on 23/07/20.
+//  Copyright © 2020 Rahul Dange. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RSGithubRepoListing.
+FOUNDATION_EXPORT double RSGithubRepoListingVersionNumber;
+
+//! Project version string for RSGithubRepoListing.
+FOUNDATION_EXPORT const unsigned char RSGithubRepoListingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSGithubRepoListing/PublicHeader.h>
+
+
